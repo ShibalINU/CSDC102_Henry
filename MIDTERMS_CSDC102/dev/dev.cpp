c905c229b8eea85179ae0583f82786b8a48774fb
ShibalINU <164317399+ShibalINU@users.noreply.github.com>
2026-03-09 16:05:55 +0800

    Update dev.cpp

--- MIDTERMS_CSDC102/dev/dev.cpp
+++ MIDTERMS_CSDC102/dev/dev.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    const int NUM_BANKS = 4;
+    string bankNames[NUM_BANKS] = {"BDO", "BPI", "Metrobank", "Security Bank"};
+    double localFees[NUM_BANKS] = {25, 20, 30, 15};
+    double intlFees[NUM_BANKS] = {150, 125, 200, 100};
+    double dailyLimits[NUM_BANKS] = {50000, 75000, 100000, 60000};
+
+    return 0;
+}
